5912dd4159454e34f11a5ed6c738c78b11b73297
Taeden (they/them) <tca0103@westminstercollege.edu>
2023-02-16 20:37:16 -0700

    dec change

--- src/edu/westminstercollege/cmpt355/minijava/Main.java
+++ src/edu/westminstercollege/cmpt355/minijava/Main.java
@@ -1,7 +1,8 @@
 package edu.westminstercollege.cmpt355.minijava;
 
 public class Main {
-
+    public double x = 1e5;
+    static final int Y = 9;
     public static void main(String... args) {
 
     }
