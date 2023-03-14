7d7cf5d9e7a00d0846c01490dbdda94ad1a45e53
Omar A <114465136+omaroaalj@users.noreply.github.com>
2023-03-14 11:48:13 -0600

    Added type variable to Variable

--- src/edu/westminstercollege/cmpt355/minijava/Variable.java
+++ src/edu/westminstercollege/cmpt355/minijava/Variable.java
@@ -2,18 +2,26 @@ package edu.westminstercollege.cmpt355.minijava;
 
 public class Variable {
 
+    private Type type;
     private String name;
     private int index;
 
-    public Variable(String name, int index) {
+    public Variable(Type type, String name, int index) {
+        this.type = type;
         this.name = name;
         this.index = index;
     }
+
+    public Type getType() { return type; }
+
+    public void setType(Type type) { this.type = type; }
 
     public String getName() {
         return name;
     }
 
     public int getIndex() { return index; }
+
+    public String toString() { return name + "[" + type.toString() + "]"; }
 
 }
