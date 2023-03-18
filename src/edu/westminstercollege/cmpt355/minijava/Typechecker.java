b1af2b474d28f664bd9e29ded864bba453b68818
Omar A <114465136+omaroaalj@users.noreply.github.com>
2023-03-18 15:25:23 -0600

    Started on typecheck method

--- src/edu/westminstercollege/cmpt355/minijava/Typechecker.java
+++ src/edu/westminstercollege/cmpt355/minijava/Typechecker.java
@@ -7,8 +7,52 @@ import java.util.List;
 
 public class Typechecker {
 
+    public void typecheck(SymbolTable symbols, Node node) throws SyntaxException {
+        switch(node) {
+            case IntLiteral(ParserRuleContext ctx2, String text) -> {
+
+            }
+            case DoubleLiteral(ParserRuleContext ctx2, String text) -> {
+
+            }
+            case BooleanLiteral(ParserRuleContext ctx2, String text) -> {
+
+            }
+            case StringLiteral(ParserRuleContext ctx2, String text) -> {
+
+            }
+            case VariableAccess(ParserRuleContext ctx, String variableName) -> {
+
+            }
+            case Assignment(ParserRuleContext ctx, Expression exprName, Expression expression) -> {
+
+            }
+            case BinaryOp(ParserRuleContext ctx1, String operator, Expression left, Expression right) -> {
+
+            }
+            case Negate(ParserRuleContext ctx, Expression expression) -> {
+
+            }
+            case PreIncrement(ParserRuleContext ctx, Expression expression, String increment) -> {
+
+            }
+            case PostIncrement(ParserRuleContext ctx, Expression expression, String increment) -> {
+
+            }
+            case Cast(ParserRuleContext ctx, TypeNode type, Expression expression) -> {
+
+            }
+            case Print(ParserRuleContext ctx, List<Expression> expressions) -> {
+
+            }
+            default -> {
+                throw new RuntimeException(String.format("Unimplemented: %s", node.getNodeDescription()));
+            }
+        }
+    }
+
     public Type getType(SymbolTable symbols, Expression expr) {
-        switch(expr){
+        switch(expr) {
             case IntLiteral(ParserRuleContext ctx2, String text) -> {
 
             }
