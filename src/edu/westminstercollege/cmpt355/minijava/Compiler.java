ae824943b4cffa56fae1e0cee6014e9922f02f05
Omar A <114465136+omaroaalj@users.noreply.github.com>
2023-04-01 10:46:05 -0600

    Completed both increments

--- src/edu/westminstercollege/cmpt355/minijava/Compiler.java
+++ src/edu/westminstercollege/cmpt355/minijava/Compiler.java
@@ -287,17 +287,50 @@ public class Compiler {
                             "Internal compiler error: type of negate is %s", exprType));
             }
             case PreIncrement(ParserRuleContext ctx, Expression expr, String increment) -> {
-                //generateCode(out, symbols, expr);
                 var varName = (VariableAccess)expr;
                 var varValue = symbols.findVariable(varName.variableName()).get();
                 var exprType = tc.getType(symbols, varName);
                 var varIndex = varValue.getIndex();
                 if (exprType == PrimitiveType.Int) {
-                    out.println(String.format("iinc %d 1", varIndex));
+                    if (increment.equals("++"))
+                        out.println(String.format("iinc %d 1", varIndex));
+                    else
+                        out.println(String.format("iinc %d -1", varIndex));
                     out.println(String.format("iload %d", varIndex));
                 } else if (exprType == PrimitiveType.Double) {
                     out.println(String.format("dload %d", varIndex));
-                    out.println(String.format("dconst_1", varIndex));
+                    out.println("dconst_1");
+                    if (increment.equals("++"))
+                        out.println("dadd");
+                    else
+                        out.println("dsub");
+                    out.println("dup2");
+                    out.println(String.format("dstore %d", varIndex));
+                }
+                else
+                    throw new RuntimeException(String.format(
+                            "Internal compiler error: type of pre-increment is %s", exprType));
+            }
+            case PostIncrement(ParserRuleContext ctx, Expression expr, String increment) -> {
+                var varName = (VariableAccess)expr;
+                var varValue = symbols.findVariable(varName.variableName()).get();
+                var exprType = tc.getType(symbols, varName);
+                var varIndex = varValue.getIndex();
+                if (exprType == PrimitiveType.Int) {
+                    out.println(String.format("iload %d", varIndex));
+                    if (increment.equals("++"))
+                        out.println(String.format("iinc %d 1", varIndex));
+                    else
+                        out.println(String.format("iinc %d -1", varIndex));
+                } else if (exprType == PrimitiveType.Double) {
+                    out.println(String.format("dload %d", varIndex));
+                    out.println("dup2");
+                    out.println("dconst_1");
+                    if (increment.equals("++"))
+                        out.println("dadd");
+                    else
+                        out.println("dsub");
+                    out.println(String.format("dstore %d", varIndex));
                 }
                 else
                     throw new RuntimeException(String.format(
