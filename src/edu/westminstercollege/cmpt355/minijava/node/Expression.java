546cb1b648f68d308cee4be06ac398fc5df0e150
Taeden (they/them) <tca0103@westminstercollege.edu>
2023-04-01 14:28:08 -0600

    added ConstructorCall node

--- src/edu/westminstercollege/cmpt355/minijava/node/Expression.java
+++ src/edu/westminstercollege/cmpt355/minijava/node/Expression.java
@@ -3,6 +3,6 @@ package edu.westminstercollege.cmpt355.minijava.node;
 public sealed interface Expression extends Node
     permits IntLiteral, DoubleLiteral, BooleanLiteral, StringLiteral,
         VariableAccess, Assignment, BinaryOp, Negate, PreIncrement,
-        PostIncrement, Cast, Print, FieldAccess, MethodCall {
+        PostIncrement, Cast, Print, FieldAccess, MethodCall, ConstructorCall {
 
 }
